3bc1f8ef8aee75bf3251e57a35777cd94c299211
Benjamin Townsend <benctownsend@gmail.com>
2025-10-06 14:52:03 +0200

    Move site.css to core/static/core/css, update base.html, clean static settings

--- shopops/settings.py
+++ shopops/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+WHITENOISE_MANIFEST_STRICT = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
