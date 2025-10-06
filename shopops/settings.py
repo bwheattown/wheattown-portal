781b862de4a36bc7735ef8710eaa5396961e2e97
Benjamin Townsend <benctownsend@gmail.com>
2025-10-06 18:11:46 +0200

    added debugging to end of settings.py

--- shopops/settings.py
+++ shopops/settings.py
@@ -141,3 +141,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SECURE_SSL_REDIRECT = not DEBUG
 SESSION_COOKIE_SECURE = not DEBUG
 CSRF_COOKIE_SECURE = not DEBUG
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "ERROR",
+    },
+}
+
